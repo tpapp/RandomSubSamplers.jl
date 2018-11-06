a9a8a99b6b74a4cead58d20819adcb09e876e0f5
Tamas K. Papp <tkpapp@gmail.com>
2018-11-06 14:18:39 +0100

    random seed fixing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,6 @@
-using RandomSubSamplers, Test
+using RandomSubSamplers, Test, Random
+
+Random.seed!(UInt32[0xb0a863be, 0x4c2f0833, 0xa08d7276, 0xb3fe604f])
 
 @testset "random subsampler" begin
     v = rand(1:100, 1000)
@@ -6,6 +8,6 @@ using RandomSubSamplers, Test
     vw = tuple.(v, w)
     rvw = collect(random_subsample(first, 0.3, vw))
     k = unique(first.(rvw))
-    @test 30 ≤ length(k)  ≤ 36
+    @test 26 ≤ length(k)  ≤ 40  # generous bounds for CI
     @test filter(x -> first(x) ∈ k, vw) == rvw
 end
